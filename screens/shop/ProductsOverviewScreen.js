9aca9d20845becc45c00cff4318dbd2817e2f0bd
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-07-29 15:57:33 +0530

    start productDetailsScreen and add touchable components

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -16,7 +16,14 @@ const ProductsOverviewScreen = props => {
                     image={itemData.item.imageUrl}
                     title={itemData.item.title}
                     price={itemData.item.price}
-                    onViewDetail={() => { }}
+                    onViewDetail={() => {
+                        props.navigation.navigate(
+                            'ProductDetail',
+                            {
+                                productId: itemData.item.id,
+                                productTitle: itemData.item.title
+                            })
+                    }}
                     onAddToCart={() => { }}
                 />
             )}

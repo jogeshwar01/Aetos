1b402ad61ec92fc0e6429c3112a2afa647b9fe16
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-07-30 17:59:21 +0530

    setup redux thunk

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import * as Font from 'expo-font';
 // import { composeWithDevTools } from 'redux-devtools-extension';  //remove during production
 import AppLoading from 'expo-app-loading';
+import ReduxThunk from 'redux-thunk';
 
 import productReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
@@ -18,7 +19,7 @@ const rootReducer = combineReducers({
 
 //to use reduv dev tools
 // const store = createStore(rootReducer, composeWithDevTools());
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({

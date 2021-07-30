bcc6946a9948b8180cdb7910b4be3a5a9d60e420
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-07-30 12:42:58 +0530

    add side drawer and orders screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,11 +1,26 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
 import Colors from '../constants/Colors'
+
+const defaultNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+}
 
 const ProductsNavigator = createStackNavigator({
     ProductsOverview: ProductsOverviewScreen,
@@ -13,19 +28,31 @@ const ProductsNavigator = createStackNavigator({
     Cart: CartScreen
 },
     {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const OrdersNavigator = createStackNavigator({
+    Orders: OrdersScreen
+},
+    {
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const ShopNavigator = createDrawerNavigator({
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator
+},
+    {
+        contentOptions: {
+            activeTintColor: Colors.primary,
+            itemsContainerStyle: {
+                marginVertical: 35,
             },
-            headerTitleStyle: {
-                fontFamily: 'open-sans-bold'
-            },
-            headerBackTitleStyle: {
-                fontFamily: 'open-sans'
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
         }
     }
 );
 
-export default createAppContainer(ProductsNavigator);
+
+export default createAppContainer(ShopNavigator);
